2cd8ee0443c5058e9fee9d9d90ffa03db705ea44
Alejandro <57674935+McFlyWare@users.noreply.github.com>
2022-09-05 19:22:21 -0500

    implementacion camara pt 1

--- electrocardio/lib/theme/theme.dart
+++ electrocardio/lib/theme/theme.dart
@@ -5,41 +5,37 @@ class ThemeApp {
   static final Color primary = Color.fromARGB(255, 65, 131, 255);
   static final Color appRed = Color.fromARGB(255, 229, 32, 32);
   static final ThemeData light = ThemeData.light().copyWith(
-      primaryColor: primary,
-      inputDecorationTheme: InputDecorationTheme(
-        floatingLabelStyle: TextStyle(
-          color: primary,
-        ),
-        enabledBorder: OutlineInputBorder(
-          borderSide: BorderSide(color: primary),
-          borderRadius: BorderRadius.circular(15),
-        ),
-        focusedBorder: OutlineInputBorder(
-          borderSide: BorderSide(color: primary),
-          borderRadius: BorderRadius.circular(15),
-        ),
-        border: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(15),
-        ),
+    primaryColor: primary,
+    inputDecorationTheme: InputDecorationTheme(
+      floatingLabelStyle: TextStyle(
+        color: primary,
       ),
-      elevatedButtonTheme: ElevatedButtonThemeData(
-        style: ElevatedButton.styleFrom(
-          primary: primary,
-          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)),
-          elevation: 10,
-        ),
+      enabledBorder: OutlineInputBorder(
+        borderSide: BorderSide(color: primary),
+        borderRadius: BorderRadius.circular(15),
       ),
-      floatingActionButtonTheme:
-          FloatingActionButtonThemeData(backgroundColor: primary),
-      textButtonTheme: TextButtonThemeData(
-        style: TextButton.styleFrom(
-          primary: primary,
-          textStyle: TextStyle(color: Colors.white),
-        ),
+      focusedBorder: OutlineInputBorder(
+        borderSide: BorderSide(color: primary),
+        borderRadius: BorderRadius.circular(15),
       ),
-      appBarTheme: AppBarTheme(
-          color: primary,
-          elevation: 0,
-          systemOverlayStyle:
-              const SystemUiOverlayStyle(statusBarColor: Colors.white)));
+      border: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(15),
+      ),
+    ),
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        primary: primary,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)),
+        elevation: 10,
+      ),
+    ),
+    floatingActionButtonTheme:
+        FloatingActionButtonThemeData(backgroundColor: primary),
+    textButtonTheme: TextButtonThemeData(
+      style: TextButton.styleFrom(
+        primary: primary,
+        textStyle: TextStyle(color: Colors.white),
+      ),
+    ),
+  );
 }
--- electrocardio/lib/widgets/electro_card.dart
+++ electrocardio/lib/widgets/electro_card.dart
@@ -1,7 +1,10 @@
+import 'dart:io';
+
 import 'package:electrocardio/theme/theme.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:image_picker/image_picker.dart';
 
 class ElectroCard extends StatefulWidget {
   ElectroCard({Key? key}) : super(key: key);
@@ -11,43 +14,72 @@ class ElectroCard extends StatefulWidget {
 }
 
 class _ElectroCardState extends State<ElectroCard> {
+  String imagePath = "assets/img/electro_placeholder.png";
+  File fileImage = new File('');
+  bool loadImage = false;
+
+  /*
+    XFile? _image;
+    Future getImage() async {
+      final image = await ImagePicker.pickImage(source: ImageSource.camera);
+      if (image == null) return;
+      _image = image;
+    }
+*/
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: const EdgeInsets.all(30),
-      elevation: 5,
-      clipBehavior: Clip.antiAliasWithSaveLayer,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(10),
-      ),
-      child: SizedBox(
-        height: 325,
-        width: MediaQuery.of(context).size.width,
-        child: Column(
-          children: [
-            const Image(
-              image: AssetImage("assets/img/electro_placeholder.png"),
-              fit: BoxFit.cover,
-              height: 250,
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            Text(
-              "Tomar foto de electrocardiograma (EGC)",
-              style: GoogleFonts.rubik(
-                color: ThemeApp.appRed,
-                fontSize: 15,
+    return GestureDetector(
+      onTap: () async {
+        XFile? image =
+            await ImagePicker().pickImage(source: ImageSource.camera);
+        setState(() {
+          if (image != null) {
+            //imagePath = image.path;
+            print(image.path);
+            fileImage = File(image.path);
+            loadImage = true;
+          }
+        });
+      },
+      child: Card(
+        margin: const EdgeInsets.all(30),
+        elevation: 5,
+        clipBehavior: Clip.antiAliasWithSaveLayer,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: SizedBox(
+          height: 325,
+          width: MediaQuery.of(context).size.width,
+          child: Column(
+            children: [
+              Image(
+                image: loadImage
+                    ? FileImage(fileImage)
+                    : AssetImage("assets/img/electro_placeholder.png")
+                        as ImageProvider,
+                fit: BoxFit.cover,
+                height: 250,
               ),
-            ),
-            Text(
-              "Por favor, intente hacer la toma lo mejor posible",
-              style: GoogleFonts.rubik(
-                color: Colors.grey,
-                fontSize: 12,
+              const SizedBox(
+                height: 20,
               ),
-            )
-          ],
+              Text(
+                "Tomar foto de electrocardiograma (EGC)",
+                style: GoogleFonts.rubik(
+                  color: ThemeApp.appRed,
+                  fontSize: 15,
+                ),
+              ),
+              Text(
+                "Por favor, intente hacer la toma lo mejor posible",
+                style: GoogleFonts.rubik(
+                  color: Colors.grey,
+                  fontSize: 12,
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
